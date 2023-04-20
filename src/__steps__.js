fa95f5db54160a16d631ab71fb76d17805070885
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-04-20 23:59:40 +0600

    Implemented login and registration

---
+++ src/__steps__.js
@@ -0,0 +1,20 @@
+/**
+ * CONTEXT API
+ * 1. Create context (export)
+ * 2. Create provider
+ * 3. Set context value
+ * 4. To access the value: useContext hook
+ * 
+ *  */
+
+/** 
+ * 1. main.jsx set AuthProvider
+ * 2. Inside provider access children props and then use
+ * 
+*/
+
+
+/**
+ * 1. Break down the create user function
+ * 2. Auth provider will have auth and pass email and password from register
+ * */ 
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLJ4i6lvxHps4VtMLGJ2SGV8r6VxWFHys",
+  authDomain: "auth-firebase-context-ta-d3455.firebaseapp.com",
+  projectId: "auth-firebase-context-ta-d3455",
+  storageBucket: "auth-firebase-context-ta-d3455.appspot.com",
+  messagingSenderId: "466036588558",
+  appId: "1:466036588558:web:72c39e9ea1f0c60bba7c5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
